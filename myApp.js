9b965d38c1964bb41e6b27646ef0627503a97f87
Anh Phat <huynht.anhphat@gmail.com>
2026-02-07 12:08:51 +0700

    Start a repository

--- myApp.js
+++ myApp.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
+
+const PORT = process.env.PORT;
+
+// your code
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
 
 
 
